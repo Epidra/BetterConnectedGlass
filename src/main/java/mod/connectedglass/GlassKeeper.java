e2d12aa849bfef53c3e5ac7c460c4bceab375858
Epidra <epidra@web.de>
2023-11-01 21:58:11 +0100

    Port to 1.19.4

--- src/main/java/mod/connectedglass/GlassKeeper.java
+++ src/main/java/mod/connectedglass/GlassKeeper.java
@@ -32,42 +32,42 @@ public class GlassKeeper {
 
 
     // Blocks
-    public static final RegistryObject<Block> BLOCK_GLASS_CLEAR      = register("glass",                    () -> new BlockGlass(       Blocks.GLASS                                   ),      CreativeModeTab.TAB_BUILDING_BLOCKS);
-    public static final RegistryObject<Block> BLOCK_GLASS_WHITE      = register("white_stained_glass",      () -> new BlockGlassStained(Blocks.WHITE_STAINED_GLASS,      DyeColor.WHITE),      CreativeModeTab.TAB_BUILDING_BLOCKS);
-    public static final RegistryObject<Block> BLOCK_GLASS_ORANGE     = register("orange_stained_glass",     () -> new BlockGlassStained(Blocks.ORANGE_STAINED_GLASS,     DyeColor.ORANGE),     CreativeModeTab.TAB_BUILDING_BLOCKS);
-    public static final RegistryObject<Block> BLOCK_GLASS_MAGENTA    = register("magenta_stained_glass",    () -> new BlockGlassStained(Blocks.MAGENTA_STAINED_GLASS,    DyeColor.MAGENTA),    CreativeModeTab.TAB_BUILDING_BLOCKS);
-    public static final RegistryObject<Block> BLOCK_GLASS_LIGHT_BLUE = register("light_blue_stained_glass", () -> new BlockGlassStained(Blocks.LIGHT_BLUE_STAINED_GLASS, DyeColor.LIGHT_BLUE), CreativeModeTab.TAB_BUILDING_BLOCKS);
-    public static final RegistryObject<Block> BLOCK_GLASS_YELLOW     = register("yellow_stained_glass",     () -> new BlockGlassStained(Blocks.YELLOW_STAINED_GLASS,     DyeColor.YELLOW),     CreativeModeTab.TAB_BUILDING_BLOCKS);
-    public static final RegistryObject<Block> BLOCK_GLASS_LIME       = register("lime_stained_glass",       () -> new BlockGlassStained(Blocks.LIME_STAINED_GLASS,       DyeColor.LIME),       CreativeModeTab.TAB_BUILDING_BLOCKS);
-    public static final RegistryObject<Block> BLOCK_GLASS_PINK       = register("pink_stained_glass",       () -> new BlockGlassStained(Blocks.PINK_STAINED_GLASS,       DyeColor.PINK),       CreativeModeTab.TAB_BUILDING_BLOCKS);
-    public static final RegistryObject<Block> BLOCK_GLASS_GRAY       = register("gray_stained_glass",       () -> new BlockGlassStained(Blocks.GRAY_STAINED_GLASS,       DyeColor.GRAY),       CreativeModeTab.TAB_BUILDING_BLOCKS);
-    public static final RegistryObject<Block> BLOCK_GLASS_LIGHT_GRAY = register("light_gray_stained_glass", () -> new BlockGlassStained(Blocks.LIGHT_GRAY_STAINED_GLASS, DyeColor.LIGHT_GRAY), CreativeModeTab.TAB_BUILDING_BLOCKS);
-    public static final RegistryObject<Block> BLOCK_GLASS_CYAN       = register("cyan_stained_glass",       () -> new BlockGlassStained(Blocks.CYAN_STAINED_GLASS,       DyeColor.CYAN),       CreativeModeTab.TAB_BUILDING_BLOCKS);
-    public static final RegistryObject<Block> BLOCK_GLASS_PURPLE     = register("purple_stained_glass",     () -> new BlockGlassStained(Blocks.PURPLE_STAINED_GLASS,     DyeColor.PURPLE),     CreativeModeTab.TAB_BUILDING_BLOCKS);
-    public static final RegistryObject<Block> BLOCK_GLASS_BLUE       = register("blue_stained_glass",       () -> new BlockGlassStained(Blocks.BLUE_STAINED_GLASS,       DyeColor.BLUE),       CreativeModeTab.TAB_BUILDING_BLOCKS);
-    public static final RegistryObject<Block> BLOCK_GLASS_BROWN      = register("brown_stained_glass",      () -> new BlockGlassStained(Blocks.BROWN_STAINED_GLASS,      DyeColor.BROWN),      CreativeModeTab.TAB_BUILDING_BLOCKS);
-    public static final RegistryObject<Block> BLOCK_GLASS_GREEN      = register("green_stained_glass",      () -> new BlockGlassStained(Blocks.GREEN_STAINED_GLASS,      DyeColor.GREEN),      CreativeModeTab.TAB_BUILDING_BLOCKS);
-    public static final RegistryObject<Block> BLOCK_GLASS_RED        = register("red_stained_glass",        () -> new BlockGlassStained(Blocks.RED_STAINED_GLASS,        DyeColor.RED),        CreativeModeTab.TAB_BUILDING_BLOCKS);
-    public static final RegistryObject<Block> BLOCK_GLASS_BLACK      = register("black_stained_glass",      () -> new BlockGlassStained(Blocks.BLACK_STAINED_GLASS,      DyeColor.BLACK),      CreativeModeTab.TAB_BUILDING_BLOCKS);
+    public static final RegistryObject<Block> BLOCK_GLASS_CLEAR      = register("glass",                    () -> new BlockGlass(       Blocks.GLASS                                   ),      true);
+    public static final RegistryObject<Block> BLOCK_GLASS_WHITE      = register("white_stained_glass",      () -> new BlockGlassStained(Blocks.WHITE_STAINED_GLASS,      DyeColor.WHITE),      true);
+    public static final RegistryObject<Block> BLOCK_GLASS_ORANGE     = register("orange_stained_glass",     () -> new BlockGlassStained(Blocks.ORANGE_STAINED_GLASS,     DyeColor.ORANGE),     true);
+    public static final RegistryObject<Block> BLOCK_GLASS_MAGENTA    = register("magenta_stained_glass",    () -> new BlockGlassStained(Blocks.MAGENTA_STAINED_GLASS,    DyeColor.MAGENTA),    true);
+    public static final RegistryObject<Block> BLOCK_GLASS_LIGHT_BLUE = register("light_blue_stained_glass", () -> new BlockGlassStained(Blocks.LIGHT_BLUE_STAINED_GLASS, DyeColor.LIGHT_BLUE), true);
+    public static final RegistryObject<Block> BLOCK_GLASS_YELLOW     = register("yellow_stained_glass",     () -> new BlockGlassStained(Blocks.YELLOW_STAINED_GLASS,     DyeColor.YELLOW),     true);
+    public static final RegistryObject<Block> BLOCK_GLASS_LIME       = register("lime_stained_glass",       () -> new BlockGlassStained(Blocks.LIME_STAINED_GLASS,       DyeColor.LIME),       true);
+    public static final RegistryObject<Block> BLOCK_GLASS_PINK       = register("pink_stained_glass",       () -> new BlockGlassStained(Blocks.PINK_STAINED_GLASS,       DyeColor.PINK),       true);
+    public static final RegistryObject<Block> BLOCK_GLASS_GRAY       = register("gray_stained_glass",       () -> new BlockGlassStained(Blocks.GRAY_STAINED_GLASS,       DyeColor.GRAY),       true);
+    public static final RegistryObject<Block> BLOCK_GLASS_LIGHT_GRAY = register("light_gray_stained_glass", () -> new BlockGlassStained(Blocks.LIGHT_GRAY_STAINED_GLASS, DyeColor.LIGHT_GRAY), true);
+    public static final RegistryObject<Block> BLOCK_GLASS_CYAN       = register("cyan_stained_glass",       () -> new BlockGlassStained(Blocks.CYAN_STAINED_GLASS,       DyeColor.CYAN),       true);
+    public static final RegistryObject<Block> BLOCK_GLASS_PURPLE     = register("purple_stained_glass",     () -> new BlockGlassStained(Blocks.PURPLE_STAINED_GLASS,     DyeColor.PURPLE),     true);
+    public static final RegistryObject<Block> BLOCK_GLASS_BLUE       = register("blue_stained_glass",       () -> new BlockGlassStained(Blocks.BLUE_STAINED_GLASS,       DyeColor.BLUE),       true);
+    public static final RegistryObject<Block> BLOCK_GLASS_BROWN      = register("brown_stained_glass",      () -> new BlockGlassStained(Blocks.BROWN_STAINED_GLASS,      DyeColor.BROWN),      true);
+    public static final RegistryObject<Block> BLOCK_GLASS_GREEN      = register("green_stained_glass",      () -> new BlockGlassStained(Blocks.GREEN_STAINED_GLASS,      DyeColor.GREEN),      true);
+    public static final RegistryObject<Block> BLOCK_GLASS_RED        = register("red_stained_glass",        () -> new BlockGlassStained(Blocks.RED_STAINED_GLASS,        DyeColor.RED),        true);
+    public static final RegistryObject<Block> BLOCK_GLASS_BLACK      = register("black_stained_glass",      () -> new BlockGlassStained(Blocks.BLACK_STAINED_GLASS,      DyeColor.BLACK),      true);
 
     // Panes
-    public static final RegistryObject<Block> BLOCK_PANE_CLEAR      = register("glass_pane",                    () -> new BlockPane(       Blocks.GLASS                                        ),      CreativeModeTab.TAB_DECORATIONS);
-    public static final RegistryObject<Block> BLOCK_PANE_WHITE      = register("white_stained_glass_pane",      () -> new BlockPaneStained(Blocks.WHITE_STAINED_GLASS_PANE,      DyeColor.WHITE),      CreativeModeTab.TAB_DECORATIONS);
-    public static final RegistryObject<Block> BLOCK_PANE_ORANGE     = register("orange_stained_glass_pane",     () -> new BlockPaneStained(Blocks.ORANGE_STAINED_GLASS_PANE,     DyeColor.ORANGE),     CreativeModeTab.TAB_DECORATIONS);
-    public static final RegistryObject<Block> BLOCK_PANE_MAGENTA    = register("magenta_stained_glass_pane",    () -> new BlockPaneStained(Blocks.MAGENTA_STAINED_GLASS_PANE,    DyeColor.MAGENTA),    CreativeModeTab.TAB_DECORATIONS);
-    public static final RegistryObject<Block> BLOCK_PANE_LIGHT_BLUE = register("light_blue_stained_glass_pane", () -> new BlockPaneStained(Blocks.LIGHT_BLUE_STAINED_GLASS_PANE, DyeColor.LIGHT_BLUE), CreativeModeTab.TAB_DECORATIONS);
-    public static final RegistryObject<Block> BLOCK_PANE_YELLOW     = register("yellow_stained_glass_pane",     () -> new BlockPaneStained(Blocks.YELLOW_STAINED_GLASS_PANE,     DyeColor.YELLOW),     CreativeModeTab.TAB_DECORATIONS);
-    public static final RegistryObject<Block> BLOCK_PANE_LIME       = register("lime_stained_glass_pane",       () -> new BlockPaneStained(Blocks.LIME_STAINED_GLASS_PANE,       DyeColor.LIME),       CreativeModeTab.TAB_DECORATIONS);
-    public static final RegistryObject<Block> BLOCK_PANE_PINK       = register("pink_stained_glass_pane",       () -> new BlockPaneStained(Blocks.PINK_STAINED_GLASS_PANE,       DyeColor.PINK),       CreativeModeTab.TAB_DECORATIONS);
-    public static final RegistryObject<Block> BLOCK_PANE_GRAY       = register("gray_stained_glass_pane",       () -> new BlockPaneStained(Blocks.GRAY_STAINED_GLASS_PANE,       DyeColor.GRAY),       CreativeModeTab.TAB_DECORATIONS);
-    public static final RegistryObject<Block> BLOCK_PANE_LIGHT_GRAY = register("light_gray_stained_glass_pane", () -> new BlockPaneStained(Blocks.LIGHT_GRAY_STAINED_GLASS_PANE, DyeColor.LIGHT_GRAY), CreativeModeTab.TAB_DECORATIONS);
-    public static final RegistryObject<Block> BLOCK_PANE_CYAN       = register("cyan_stained_glass_pane",       () -> new BlockPaneStained(Blocks.CYAN_STAINED_GLASS_PANE,       DyeColor.CYAN),       CreativeModeTab.TAB_DECORATIONS);
-    public static final RegistryObject<Block> BLOCK_PANE_PURPLE     = register("purple_stained_glass_pane",     () -> new BlockPaneStained(Blocks.PURPLE_STAINED_GLASS_PANE,     DyeColor.PURPLE),     CreativeModeTab.TAB_DECORATIONS);
-    public static final RegistryObject<Block> BLOCK_PANE_BLUE       = register("blue_stained_glass_pane",       () -> new BlockPaneStained(Blocks.BLUE_STAINED_GLASS_PANE,       DyeColor.BLUE),       CreativeModeTab.TAB_DECORATIONS);
-    public static final RegistryObject<Block> BLOCK_PANE_BROWN      = register("brown_stained_glass_pane",      () -> new BlockPaneStained(Blocks.BROWN_STAINED_GLASS_PANE,      DyeColor.BROWN),      CreativeModeTab.TAB_DECORATIONS);
-    public static final RegistryObject<Block> BLOCK_PANE_GREEN      = register("green_stained_glass_pane",      () -> new BlockPaneStained(Blocks.GREEN_STAINED_GLASS_PANE,      DyeColor.GREEN),      CreativeModeTab.TAB_DECORATIONS);
-    public static final RegistryObject<Block> BLOCK_PANE_RED        = register("red_stained_glass_pane",        () -> new BlockPaneStained(Blocks.RED_STAINED_GLASS_PANE,        DyeColor.RED),        CreativeModeTab.TAB_DECORATIONS);
-    public static final RegistryObject<Block> BLOCK_PANE_BLACK      = register("black_stained_glass_pane",      () -> new BlockPaneStained(Blocks.BLACK_STAINED_GLASS_PANE,      DyeColor.BLACK),      CreativeModeTab.TAB_DECORATIONS);
+    public static final RegistryObject<Block> BLOCK_PANE_CLEAR      = register("glass_pane",                    () -> new BlockPane(       Blocks.GLASS                                        ),      true);
+    public static final RegistryObject<Block> BLOCK_PANE_WHITE      = register("white_stained_glass_pane",      () -> new BlockPaneStained(Blocks.WHITE_STAINED_GLASS_PANE,      DyeColor.WHITE),      true);
+    public static final RegistryObject<Block> BLOCK_PANE_ORANGE     = register("orange_stained_glass_pane",     () -> new BlockPaneStained(Blocks.ORANGE_STAINED_GLASS_PANE,     DyeColor.ORANGE),     true);
+    public static final RegistryObject<Block> BLOCK_PANE_MAGENTA    = register("magenta_stained_glass_pane",    () -> new BlockPaneStained(Blocks.MAGENTA_STAINED_GLASS_PANE,    DyeColor.MAGENTA),    true);
+    public static final RegistryObject<Block> BLOCK_PANE_LIGHT_BLUE = register("light_blue_stained_glass_pane", () -> new BlockPaneStained(Blocks.LIGHT_BLUE_STAINED_GLASS_PANE, DyeColor.LIGHT_BLUE), true);
+    public static final RegistryObject<Block> BLOCK_PANE_YELLOW     = register("yellow_stained_glass_pane",     () -> new BlockPaneStained(Blocks.YELLOW_STAINED_GLASS_PANE,     DyeColor.YELLOW),     true);
+    public static final RegistryObject<Block> BLOCK_PANE_LIME       = register("lime_stained_glass_pane",       () -> new BlockPaneStained(Blocks.LIME_STAINED_GLASS_PANE,       DyeColor.LIME),       true);
+    public static final RegistryObject<Block> BLOCK_PANE_PINK       = register("pink_stained_glass_pane",       () -> new BlockPaneStained(Blocks.PINK_STAINED_GLASS_PANE,       DyeColor.PINK),       true);
+    public static final RegistryObject<Block> BLOCK_PANE_GRAY       = register("gray_stained_glass_pane",       () -> new BlockPaneStained(Blocks.GRAY_STAINED_GLASS_PANE,       DyeColor.GRAY),       true);
+    public static final RegistryObject<Block> BLOCK_PANE_LIGHT_GRAY = register("light_gray_stained_glass_pane", () -> new BlockPaneStained(Blocks.LIGHT_GRAY_STAINED_GLASS_PANE, DyeColor.LIGHT_GRAY), true);
+    public static final RegistryObject<Block> BLOCK_PANE_CYAN       = register("cyan_stained_glass_pane",       () -> new BlockPaneStained(Blocks.CYAN_STAINED_GLASS_PANE,       DyeColor.CYAN),       true);
+    public static final RegistryObject<Block> BLOCK_PANE_PURPLE     = register("purple_stained_glass_pane",     () -> new BlockPaneStained(Blocks.PURPLE_STAINED_GLASS_PANE,     DyeColor.PURPLE),     true);
+    public static final RegistryObject<Block> BLOCK_PANE_BLUE       = register("blue_stained_glass_pane",       () -> new BlockPaneStained(Blocks.BLUE_STAINED_GLASS_PANE,       DyeColor.BLUE),       true);
+    public static final RegistryObject<Block> BLOCK_PANE_BROWN      = register("brown_stained_glass_pane",      () -> new BlockPaneStained(Blocks.BROWN_STAINED_GLASS_PANE,      DyeColor.BROWN),      true);
+    public static final RegistryObject<Block> BLOCK_PANE_GREEN      = register("green_stained_glass_pane",      () -> new BlockPaneStained(Blocks.GREEN_STAINED_GLASS_PANE,      DyeColor.GREEN),      true);
+    public static final RegistryObject<Block> BLOCK_PANE_RED        = register("red_stained_glass_pane",        () -> new BlockPaneStained(Blocks.RED_STAINED_GLASS_PANE,        DyeColor.RED),        true);
+    public static final RegistryObject<Block> BLOCK_PANE_BLACK      = register("black_stained_glass_pane",      () -> new BlockPaneStained(Blocks.BLACK_STAINED_GLASS_PANE,      DyeColor.BLACK),      true);
 
 
 
@@ -80,9 +80,9 @@ public class GlassKeeper {
         ITEMS.register(     FMLbus);
     }
 
-    private static RegistryObject<Block> register(String name, java.util.function.Supplier<? extends Block> block, CreativeModeTab CreativeModeTab){
+    private static RegistryObject<Block> register(String name, java.util.function.Supplier<? extends Block> block, boolean b){
         RegistryObject<Block> rob = BLOCKS.register(name, block);
-        if(CreativeModeTab != null){ ITEMS.register(name, () -> new BlockItem(rob.get(), (new Item.Properties()).tab(CreativeModeTab))); }
+        if(b){ ITEMS.register(name, () -> new BlockItem(rob.get(), (new Item.Properties()))); }
         return rob;
     }
 
