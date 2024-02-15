0cb3080222c11bf7d5b9b506677199920966030e
Epidra <epidra@web.de>
2024-02-15 09:40:01 +0100

    Grand ReFactor

--- src/main/java/mod/connectedglass/ConnectedGlass.java
+++ src/main/java/mod/connectedglass/ConnectedGlass.java
@@ -1,29 +1,46 @@
 package mod.connectedglass;
 
-import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
+// The value here should match an entry in the META-INF/mods.toml file
 @Mod("connectedglass")
 public class ConnectedGlass {
-
-    public static final String MODID = "connectedglass";
-
-
-
-
-
-    //----------------------------------------CONSTRUCTOR----------------------------------------//
-
-    public ConnectedGlass() {
-        MinecraftForge.EVENT_BUS.register(this);
-        GlassKeeper.register(FMLJavaModLoadingContext.get().getModEventBus());
-    }
-
-
-
+	
+	// Define mod id in a common place for everything to reference
+	public static final String MODID = "connectedglass";
+	
+	
+	
+	
+	
+	// ---------- ---------- ---------- ----------  CONSTRUCTOR  ---------- ---------- ---------- ---------- //
+	
+	public ConnectedGlass() {
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setupCommon);
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setupClient);
+		
+		MinecraftForge.EVENT_BUS.register(this);
+		Register.register(FMLJavaModLoadingContext.get().getModEventBus());
+	}
+	
+	
+	
+	
+	
+	// ---------- ---------- ---------- ----------  SETUP  ---------- ---------- ---------- ---------- //
+	
+	private void setupCommon(final FMLCommonSetupEvent event) {
+		Register.setup(event);
+	}
+	
+	private void setupClient(final FMLClientSetupEvent event) {
+		Register.setup(event);
+	}
+	
+	
+	
 }
---
+++ src/main/java/mod/connectedglass/common/block/BlockPaneStained.java
@@ -0,0 +1,49 @@
+package mod.connectedglass.common.block;
+
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.DyeColor;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.StainedGlassPaneBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+public class BlockPaneStained extends StainedGlassPaneBlock {
+	
+	// ...
+	
+	
+	
+	
+	
+	// ---------- ---------- ---------- ----------  CONSTRUCTOR  ---------- ---------- ---------- ---------- //
+	
+	/** Contructor with predefined BlockProperty */
+	public BlockPaneStained(Block block, DyeColor dyecolor) {
+		super(dyecolor, Properties.copy(block));
+	}
+	
+	
+	
+	
+	
+	// ---------- ---------- ---------- ----------  SUPPORT  ---------- ---------- ---------- ---------- //
+	
+	@OnlyIn(Dist.CLIENT)
+	public boolean skipRendering(BlockState state, BlockState adjacentBlockState, Direction side) {
+		if ((adjacentBlockState.getBlock() instanceof BlockPane || adjacentBlockState.getBlock() instanceof BlockPaneStained)) {
+			if (!side.getAxis().isHorizontal()) {
+				return true;
+			}
+			
+			if (state.getValue(PROPERTY_BY_DIRECTION.get(side)) && adjacentBlockState.getValue(PROPERTY_BY_DIRECTION.get(side.getOpposite()))) {
+				return true;
+			}
+		}
+		
+		return super.skipRendering(state, adjacentBlockState, side);
+	}
+	
+	
+	
+}
